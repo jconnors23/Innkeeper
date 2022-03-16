a4ac3df8ea6aa711c218cc6363ee31f319856f20
Josiah Connors <jconn23@live.unc.edu>
2022-03-16 18:38:19 -0400

    Error handling
    double sided cards, any errors, ! prefixing for case sensitivity

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import djs from 'discord.js';
-import http from 'http'; 
-import axios from 'axios'; 
+import http from 'http';
+import axios from 'axios';
 require('dotenv').config();
 
 const client = new djs.Client({
@@ -14,28 +14,50 @@ client.on('ready', () => {   // when bot is ready, listening to user tags
 // rec msg, filter msg (look for symbols), pull cards from it, if cards, take cards -> scryfall, what scryfall returns, respond to user with that img
 
 client.on('messageCreate', async (message) => {
-    const queries = Array.from(message.content.matchAll(/#(.*?)#/gi)).map(match => match[1])  
-    //  regex ? = grab locals (lazy), g  = global, i = case insensitive .=anychar .map =  [ 1, 3, 4, ] [ 2, 3, 4,] , 1 grp/query at a time
-    if (queries.length == 0) { return }
-    const query = queries[0]; // get the first request
-    //@ts-ignore
-    await message.channel.sendTyping()
-    const response = await axios.get('https://api.scryfall.com/cards/search?q=' + query); 
-    const card = response.data.data[0]; // gets the first card
-    const cardimage = card.image_uris.border_crop;
-    message.reply({embeds: [new djs.MessageEmbed().setImage(cardimage).setFields([
-        {
-        name: '$',
-        value: card.prices.usd,
-        inline: true
-    }, {
-        name: '🌟',
-        value: card.prices.usd_foil,
-        inline: true
-    }].filter(field => field.value).map((field) => {              // filter null prices   
-        field.value = `[${field.value}](${card.purchase_uris.tcgplayer})`          // fetch tcg player link 
-        return field
-    }))]}) 
+    try {
+        const queries = Array.from(message.content.matchAll(/#(.*?)#/gi)).map(match => match[1])
+        //  regex ? = grab locals (lazy), g  = global, i = case insensitive .=anychar .map =  [ 1, 3, 4, ] [ 2, 3, 4,] , 1 grp/query at a time
+        if (queries.length == 0) { return }
+        const query = queries[0]; // get the first request
+        //@ts-ignore
+        await message.channel.sendTyping()
+        let response;
+        try {
+            response = await axios.get('https://api.scryfall.com/cards/search?q=' + "!" + "\"" + query + "\"")
+        } catch (error: any) {
+            message.reply(error.response.data.details);
+            return;
+        }
+        const card = response.data.data[0]; // gets the first card
+        const cardimages = [];
+        if (card.image_uris) {
+            cardimages.push(card.image_uris.border_crop);
+        } else {
+            cardimages.push(card.card_faces![0].image_uris!.small, card.card_faces![1].image_uris!.small);
+        }
+        const fields = [new djs.MessageEmbed().setFields([
+            {
+                name: '$',
+                value: card.prices.usd,
+                inline: true
+            }, {
+                name: '🌟',
+                value: card.prices.usd_foil,
+                inline: true
+            }].filter(field => field.value).map((field) => {              // filter null prices   
+                field.value = `[${field.value}](${card.purchase_uris.tcgplayer})`          // fetch tcg player link 
+                return field
+            }))]
+        if (cardimages[0]) {
+            fields[0].setImage(cardimages[0])
+        }
+        if (cardimages[1]) {
+            fields.push(new djs.MessageEmbed().setImage(cardimages[1]))
+        }
+        message.reply({ embeds: fields })
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 client.login(process.env.discord_token) // tell the bot to login 
@@ -43,3 +65,4 @@ client.login(process.env.discord_token) // tell the bot to login
 http.createServer((request, response) => { // make heroku realize app is active
     response.writeHead(200).end()
 }).listen(process.env.PORT || 3000)
+
