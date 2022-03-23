087d6f67e0ecc63c11ccbeb689fc72c538bfc582
Josiah Connors <jconn23@live.unc.edu>
2022-03-23 16:36:57 -0400

    comments

--- index.ts
+++ index.ts
@@ -4,36 +4,36 @@ import axios from 'axios';
 require('dotenv').config();
 
 const client = new djs.Client({
-    intents: [djs.Intents.FLAGS.GUILDS, djs.Intents.FLAGS.GUILD_MESSAGES] // what the bot needs, explicit 
+    intents: [djs.Intents.FLAGS.GUILDS, djs.Intents.FLAGS.GUILD_MESSAGES] // bot requirements 
 })
 
 client.on('ready', () => {   // when bot is ready, listening to user tags 
     console.log(client.user?.tag)
 })
 
-// rec msg, filter msg (look for symbols), pull cards from it, if cards, take cards -> scryfall, what scryfall returns, respond to user with that img
+// rec msg, filter msg (look for symbols), pull cards from it, if cards, take cards -> scryfall, respond to user with scryfall img
 
 client.on('messageCreate', async (message) => {
     try {
         const queries = Array.from(message.content.matchAll(/#(.*?)#/gi)).map(match => match[1])
-        //  regex ? = grab locals (lazy), g  = global, i = case insensitive .=anychar .map =  [ 1, 3, 4, ] [ 2, 3, 4,] , 1 grp/query at a time
+        //  regex ? = grab locals (lazy), g  = global, i = case insensitive .= anychar .map =  [ 1, 3, 4, ] [ 2, 3, 4,] , 1 grp/query at a time
         if (queries.length == 0) { return }
         const query = queries[0]; // get the first request
         //@ts-ignore
-        await message.channel.sendTyping()
+        await message.channel.sendTyping(); // ui 
         let response;
         try {
-            response = await axios.get('https://api.scryfall.com/cards/search?q=' + "!" + "\"" + query + "\"")
+            response = await axios.get('https://api.scryfall.com/cards/search?q=' + "!" + "\"" + query + "\"") // prefix with ! mark to return specific card with name
         } catch (error: any) {
             message.reply(error.response.data.details);
             return;
         }
-        const card = response.data.data[0]; // gets the first card
+        const card = response.data.data[0];
         const cardimages = [];
         if (card.image_uris) {
             cardimages.push(card.image_uris.border_crop);
         } else {
-            cardimages.push(card.card_faces![0].image_uris!.small, card.card_faces![1].image_uris!.small);
+            cardimages.push(card.card_faces![0].image_uris!.small, card.card_faces![1].image_uris!.small); // card img arr for double sided cards  
         }
         const fields = [new djs.MessageEmbed().setFields([
             {
@@ -44,8 +44,8 @@ client.on('messageCreate', async (message) => {
                 name: '🌟',
                 value: card.prices.usd_foil,
                 inline: true
-            }].filter(field => field.value).map((field) => {              // filter null prices   
-                field.value = `[${field.value}](${card.purchase_uris.tcgplayer})`          // fetch tcg player link 
+            }].filter(field => field.value).map((field) => { // filter null prices   
+                field.value = `[${field.value}](${card.purchase_uris.tcgplayer})` // fetch tcg player link 
                 return field
             }))]
         if (cardimages[0]) {
