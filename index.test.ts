ef7f9fc607181f0aecf2668e2fa63d8b61172144
Josiah Connors <jconn23@live.unc.edu>
2022-10-07 14:00:51 -0400

    additional unit tests, sinon install

--- index.test.ts
+++ index.test.ts
@@ -1,15 +1,35 @@
 import assert from 'assert'
-import {FetchCard} from './index'
+import { MessageEmbed } from 'discord.js'
+import {Embeds, FetchCard} from './index'
+import sinon from 'sinon'
+import axios from 'axios'
 
 describe ('fetch', () => {
     it ('retrieve', async () => {
+        const get = sinon.stub(axios, 'get').resolves({data: { data: [{ name: 'Snapcaster Mage' }]}});
         const card = await FetchCard('Snapcaster Mage');
+        assert.deepEqual(get.firstCall.args, ['https://api.scryfall.com/cards/search?q=!"Snapcaster Mage"']);
         assert.deepEqual(card.name, 'Snapcaster Mage'); 
     })
 })
 
-describe ('fields', () => {
-    it ('embeds', async () => {
-        const embed = await 
-    }
+describe ('embeds', () => {
+    it ('one', () => {
+        const embed = Embeds({ image_uris: { border_crop: 'abc'}, prices: { } });
+        assert.deepEqual(embed, [new MessageEmbed().setImage('abc')]);
+    })
+
+    it ('two', () => {
+        const embed = Embeds({ card_faces: [{image_uris: { border_crop: 'abc'}}, {image_uris: { border_crop: 'def'}}], prices: { } });
+        assert.deepEqual(embed, [new MessageEmbed().setImage('abc'), new MessageEmbed().setImage('def')]);
+    })
+
+    it ('one_prices', () => {
+        const embed = Embeds({ image_uris: { border_crop: 'abc'}, prices: {usd: 10}, purchase_uris: {tcgplayer: 'abc'} });
+        assert.deepEqual(embed, [new MessageEmbed().setImage('abc').setFields({inline: true, name: '$', value:'[10](abc)'})]);
+    })
+})
+
+afterEach(() => {
+    sinon.restore()
 })
--- index.ts
+++ index.ts
@@ -34,36 +34,41 @@ client.on('messageCreate', async (message) => {
             message.reply(error);
             return;
         }
-        const cardimages = [];
-        if (card.image_uris) {
-            cardimages.push(card.image_uris.border_crop);
-        } else {
-            cardimages.push(card.card_faces![0].image_uris!.border_crop, card.card_faces![1].image_uris!.border_crop); // logic for double sided cards
-        }
-        const fields = [new djs.MessageEmbed().setFields([
-            {
-                name: '$',
-                value: card.prices.usd,
-                inline: true
-            }, {
-                name: '🌟',
-                value: card.prices.usd_foil,
-                inline: true
-            }].filter(field => field.value).map((field) => { // filter null prices   
-                field.value = `[${field.value}](${card.purchase_uris.tcgplayer})` // template literal
-                return field
-            }))]
-        if (cardimages[0]) {
-            fields[0].setImage(cardimages[0])
-        }
-        if (cardimages[1]) {
-            fields.push(new djs.MessageEmbed().setImage(cardimages[1]))
-        }
+        let fields = Embeds(card); 
         message.reply({ embeds: fields }) // outputing field with card price & image
     } catch (error) {
         console.log(error)
     }
 })
+
+export function Embeds(card: any) {
+    const cardimages = [];
+    if (card.image_uris) {
+        cardimages.push(card.image_uris.border_crop);
+    } else {
+        cardimages.push(card.card_faces![0].image_uris!.border_crop, card.card_faces![1].image_uris!.border_crop); // logic for double sided cards
+    }
+    const fields = [new djs.MessageEmbed().setFields([
+        {
+            name: '$',
+            value: card.prices.usd,
+            inline: true
+        }, {
+            name: '🌟',
+            value: card.prices.usd_foil,
+            inline: true
+        }].filter(field => field.value).map((field) => { // filter null prices   
+            field.value = `[${field.value}](${card.purchase_uris.tcgplayer})` // template literal
+            return field
+        }))]
+    if (cardimages[0]) {
+        fields[0].setImage(cardimages[0])
+    }
+    if (cardimages[1]) {
+        fields.push(new djs.MessageEmbed().setImage(cardimages[1]))
+    }
+    return fields; 
+}
 
 if (require.main === module) {
     client.login(process.env.discord_token) 
